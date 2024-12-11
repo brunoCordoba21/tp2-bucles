46b35288ef904dedefcbe7f09a507cd8cb9a331a
Bruno Cordoba <cordobabruno32@gmail.com>
2024-12-11 12:26:07 -0300

    se realizo el ejercicio7 del tp

---
+++ js/07-ejercicio7.js
@@ -0,0 +1,18 @@
+
+// 7- Haz un script que escriba una pirámide inversa de los números del 1 al número que indique el usuario (no mayor de 50)  de la siguiente forma : (suponiendo que indica 30).
+
+
+
+let numero1 = 30
+let fila2 = 30
+
+while(numero1 >= 1){
+    let filaActualizada2 = '' 
+for(let i = 1; i <=fila2; i ++ ){
+filaActualizada2 += fila2
+}
+
+console.log(filaActualizada2)
+numero1--
+fila2 --
+}
