31b96d85f62b2d6d2153144a432752dd81e20bfc
Bruno Cordoba <cordobabruno32@gmail.com>
2024-12-11 11:58:37 -0300

    se realizo el ejercicio 4 del tp

---
+++ js/04-ejercicio4.js
@@ -0,0 +1,21 @@
+// 4- Realiza un script que pida números hasta que se pulse “cancelar”. Si no es un número deberá indicarse con un «alert» y seguir pidiendo números.
+//  Al salir con “cancelar” deberá indicarse la suma total de los números introducidos.
+
+let sumaTotal = 0;
+
+while (true) {
+    let numero = prompt("Ingrese un número (pulse 'Cancelar' para finalizar):");
+
+    if (numero === null) {
+        break;
+    } else if (numero.trim() === "") {
+        alert("Por favor, ingrese un número válido.");
+    } else if (isNaN(numero)) {
+        alert("Por favor, ingrese un número válido.");
+    } else {
+        let numeroValido = parseFloat(numero);
+        sumaTotal += numeroValido;
+    }
+}
+
+document.write("La suma total de los números es: " + sumaTotal);
