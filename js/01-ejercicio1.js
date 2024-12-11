0e1f4e429f8f88eabbcef13027c5b1e392519fe4
Bruno Cordoba <cordobabruno32@gmail.com>
2024-12-11 11:00:19 -0300

    se realizo el ejercicio1 del tp

--- js/01-ejercicio1.js
+++ js/01-ejercicio1.js
@@ -1,3 +1,17 @@
-// 1- Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje que 
+// 1- Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje que
 // ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje.
 
+let edad = prompt("Ingrese su edad:");
+
+// Validar si la entrada es null, undefined o no es un número
+if (edad === null || edad === undefined || isNaN(edad) || edad.trim() === "") {
+  alert("Por favor, ingrese un número válido.");
+} else if (edad >= 18 && edad <= 75) {
+  document.write("Ya puede conducir.");
+} else if (edad === 17) {
+  document.write("Puede conducir con permiso de los padres.");
+} else if (edad < 16) {
+  document.write("No puede conducir.");
+} else {
+  document.write("Ingrese una edad válida entre 0 y 75.");
+}
