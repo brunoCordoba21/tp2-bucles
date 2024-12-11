1bcef18dbabe459214af1c564782662943764bde
Bruno Cordoba <cordobabruno32@gmail.com>
2024-12-11 14:49:34 -0300

    se modifico el ejercicio 8, para mmostrar el programa dentro del elemento html

--- js/08-ejercicio8.js
+++ js/08-ejercicio8.js
@@ -7,18 +7,18 @@
 // 12345
 // 123456
 // ……
- let numeroDePiramides = (parseInt(prompt('Ingresar números no mayor a 50')))
-if(numeroDePiramides <= 0 || numeroDePiramides > 50 || isNaN(numeroDePiramides) || numeroDePiramides === null || numeroDePiramides === parseFloat){
+let numeroDePiramides = (parseInt(prompt('Ingresar números no mayor a 50')))
+if (numeroDePiramides <= 0 || numeroDePiramides > 50 || isNaN(numeroDePiramides) || numeroDePiramides === null || numeroDePiramides === parseFloat) {
     alert(`Lo que ingresaste ${numeroDePiramides}, no es valido ingresa un numero del 1 al 50`)
-}else{
-    for(let fila = 1 ; fila <= numeroDePiramides; fila ++ ){
-        let numerosPiramidesFinal = '' 
-        for(let numeros = 1 ; numeros <= fila; numeros ++){ 
-        numerosPiramidesFinal += numeros
+} else {
+    for (let fila = 1; fila <= numeroDePiramides; fila++) {
+        let numerosPiramidesFinal = ''
+        for (let numeros = 1; numeros <= fila; numeros++) {
+            numerosPiramidesFinal += numeros
         }
-        console.log(numerosPiramidesFinal);
+        document.write(numerosPiramidesFinal  + '<br>');
         
-        }
+    }
 }
 
 
