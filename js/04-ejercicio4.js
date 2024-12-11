8c4f3c51c92cbe7bcb08c24e8386c4fede5aef1b
Bruno Cordoba <cordobabruno32@gmail.com>
2024-12-11 12:13:08 -0300

    se realizo el ejercicio5

--- js/04-ejercicio4.js
+++ js/04-ejercicio4.js
@@ -1,6 +1,7 @@
 // 4- Realiza un script que pida números hasta que se pulse “cancelar”. Si no es un número deberá indicarse con un «alert» y seguir pidiendo números.
 //  Al salir con “cancelar” deberá indicarse la suma total de los números introducidos.
 
+
 let sumaTotal = 0;
 
 while (true) {
